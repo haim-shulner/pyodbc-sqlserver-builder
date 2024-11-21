6c15fe3393d5a5cef52dec3942500d1be2c1e30b
Haim Shulner <haim.shulner@gmail.com>
2024-11-21 16:52:28 +0200

    setup and toml

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup()
+setup(
+    name="pyodbc_package",
+    version="0.1.0",
+    description="pyodbc package with embedded ODBC drivers",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "pyodbc_package": ["bin/linux/*", "bin/windows/*", "bin/macos/*"]
+    },
+    install_requires=["pyodbc"],
+)
